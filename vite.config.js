e1dcaf95238a615603d9d2c867101be75cc6c18b
Azarattum <azarattum@pm.me>
2022-07-07 00:09:33 +0700

    Added vitest support with coverage

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,10 @@ const transformer = { ...loader({ typescript }), enforce: "pre" };
 
 export default defineConfig({
   plugins: [transformer],
+  test: {
+    exclude: ["node_modules", "build", ".svelte-kit"],
+    coverage: {
+      reporter: ["lcovonly", "text"],
+    },
+  },
 });
