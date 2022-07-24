e20f0b7ac4f29b72fbeec89c284efa2898257ef8
Azarattum <azarattum@pm.me>
2022-07-24 19:26:54 +0700

    Fixed eslint validation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
     node: true,
   },
   rules: {
+    "no-var": "error",
     "no-empty": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-function": "off",
