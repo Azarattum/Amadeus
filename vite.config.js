374522aeed23c74b2831d4f969dc18bc5f2e7015
Azarattum <azarattum@pm.me>
2022-07-25 17:52:23 +0700

    Moved away from typescript-is. Added zod & gun

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { sveltekit } from "@sveltejs/kit/vite";
-import loader from "rollup-plugin-typescript2";
-import typescript from "ttypescript";
 import { defineConfig } from "vite";
 
-const transformer = { ...loader({ typescript }), enforce: "pre" };
 const addon = {
   node: { ...nodeResolve(), enforce: "pre" },
   svelte: sveltekit(),
 };
 
 export default defineConfig(({ mode }) => ({
-  plugins: [transformer, addon[mode]],
+  plugins: [addon[mode]],
   test: {
     exclude: ["node_modules", "build", ".svelte-kit"],
     coverage: {
