ab6c522925658045260fc9e8ea9280f84a80fc31
Azarattum <azarattum@pm.me>
2022-07-28 17:21:02 +0700

    Added a temporary index page example for web

---
+++ apps/web/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Button } from "@amadeus/ui/primitive";
+</script>
+
+<!-- ///Temporary example!!! -->
+<h1 class="font-extrabold text-emerald-600">hello</h1>
+<Button>Click Me!</Button>
