875aab5913bf80c204a0d36f22523757c6137389
Azarattum <azarattum@pm.me>
2022-11-23 18:57:24 +0700

    Added an http sdk

---
+++ apps/core/network/http.ts
@@ -0,0 +1,28 @@
+import { createServer, Server } from "node:http";
+import { err, info } from "../status/log";
+import { init, stop } from "../event";
+import { async } from "libfun";
+
+let server: Server | undefined;
+let port: number | undefined;
+init((config) => {
+  port = config.port;
+  server = createServer();
+});
+
+stop(function* () {
+  if (!server || !server.listening) return;
+  info("Stopping the HTTP server...");
+  server.closeAllConnections();
+  yield* async(new Promise((resolve) => server?.close(resolve)));
+});
+
+export function http() {
+  if (!server || !port) throw new Error("The server was not initialized!");
+  if (!server.listening) {
+    info(`HTTP server is listening on port ${port}.`);
+    server.on("error", err);
+    server.listen(port);
+  }
+  return server;
+}
