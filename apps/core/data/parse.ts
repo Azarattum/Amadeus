bc8a7d81fab0886005d9fe8394a92277eb13b5f0
Azarattum <azarattum@pm.me>
2022-11-26 02:11:18 +0700

    Added uuid parse utility

---
+++ apps/core/data/parse.ts
@@ -0,0 +1,14 @@
+import type { Track } from "@amadeus-music/protocol";
+import { pipeline } from "libfun";
+
+/// NOT USED! (yet)
+const uuid = pipeline(
+  (x: Track) => `${x.artists.join(",")}-${x.title}-${x.album}`,
+  (x) => x.toLowerCase().replace(/\s/g, " "),
+  (x) => new TextEncoder().encode(x),
+  (x) => crypto.subtle.digest("SHA-1", x),
+  (x) => Array.from(new Uint8Array(x)),
+  (x) => x.map((b) => b.toString(16).padStart(2, "0")).join("")
+);
+
+export { uuid };
