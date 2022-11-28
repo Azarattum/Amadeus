1aa1cc0a8e27e76f7e7e407806cbb18f9ffaf570
Azarattum <azarattum@pm.me>
2022-11-28 23:27:03 +0700

    Ported parsing module from AmadeusCore

---
+++ apps/core/data/infer.test.ts
@@ -0,0 +1,235 @@
+import { inferTrack, inferArtists } from "./infer";
+import { it, expect } from "vitest";
+
+it("infers tracks", () => {
+  let val = inferTrack(
+    "Game of Thrones Theme Song - Karliene Version Cover (Oh La Lau) (Lyrics)"
+  );
+  expect(val.title).toBe("Game of Thrones Theme Song (Oh La Lau)");
+  expect(val.artists).toEqual(["Karliene"]);
+
+  val = inferTrack(
+    "Lau - Game of Thrones Theme - Karliene Version Cover (Audio Only)"
+  );
+  expect(val.title).toBe("Game of Thrones Theme");
+  expect(val.artists).toEqual(["Lau", "Karliene"]);
+
+  val = inferTrack(
+    "Karliene & Celtic Borders - You Win or You Die - Game of Thrones"
+  );
+  expect(val.title).toBe("You Win or You Die");
+  expect(val.artists).toEqual(["Karliene", "Celtic Borders"]);
+  expect(val.album).toBe("Game of Thrones");
+
+  val = inferTrack('"Pollution" by Tom Lehrer');
+  expect(val.title).toBe("Pollution");
+  expect(val.artists).toEqual(["Tom Lehrer"]);
+
+  val = inferTrack("Man with no name - Teleport (Original mix). HQ");
+  expect(val.title).toBe("Teleport");
+  expect(val.artists).toEqual(["Man with no name"]);
+
+  val = inferTrack("Varg  — Under Beige Nylon");
+  expect(val.title).toBe("Under Beige Nylon");
+  expect(val.artists).toEqual(["Varg"]);
+
+  val = inferTrack("varg - under beige nylon - 46bpm");
+  expect(val.title).toBe("under beige nylon");
+  expect(val.artists).toEqual(["varg"]);
+
+  val = inferTrack("Falling in drop C.");
+  expect(val.title).toBe("Falling in drop C");
+  expect(val.artists).toEqual([]);
+
+  val = inferTrack("Voodoo People - Quadsep - 1995");
+  expect(val.year).toBe(1995);
+  expect(val.title).toBe("Quadsep");
+  expect(val.artists).toEqual(["Voodoo People"]);
+
+  val = inferTrack("Teste - The Wipe (5am Synaptic) - Plus 8 Records - 1992");
+  expect(val.year).toBe(1992);
+  expect(val.title).toBe("The Wipe (5am Synaptic)");
+  expect(val.artists).toEqual(["Teste"]);
+  expect(val.album).toBe("Plus 8 Records");
+
+  val = inferTrack(
+    "Varg | I Did Not Always Appear This Way [Ascetic House 2015]"
+  );
+  expect(val.year).toBe(2015);
+  expect(val.title).toBe("I Did Not Always Appear This Way");
+  expect(val.artists).toEqual(["Varg"]);
+
+  val = inferTrack("Pig&Dan -The Saint Job San (Lee Van Dowski Remix)");
+  expect(val.title).toBe("The Saint Job San");
+  expect(val.artists).toEqual(["Pig", "Dan", "Lee Van Dowski"]);
+
+  val = inferTrack(
+    "Ambi Sessions 12/11 {Ambient Techno-Tribal-Dub Techno-Meditative}"
+  );
+  expect(val.title).toBe("Ambi Sessions 12/11");
+  expect(val.artists).toEqual([]);
+
+  val = inferTrack("PILLDRIVER // PITCH HIKER");
+  expect(val.title).toBe("PITCH HIKER");
+  expect(val.artists).toEqual(["PILLDRIVER"]);
+
+  val = inferTrack("Wu-Tang Clan -- One Blood instrumental");
+  expect(val.title).toBe("One Blood instrumental");
+  expect(val.artists).toEqual(["Wu-Tang Clan"]);
+
+  val = inferTrack('Mobb Deep "Peer Pressure"');
+  expect(val.title).toBe("Peer Pressure");
+  expect(val.artists).toEqual(["Mobb Deep"]);
+
+  val = inferTrack("The Prodigy - Voodoo People ( Parasense Rmx )");
+  expect(val.title).toBe("Voodoo People");
+  expect(val.artists).toEqual(["The Prodigy", "Parasense"]);
+
+  val = inferTrack('Giselle "Silk" Favored Nations Remix');
+  expect(val.title).toBe("Silk");
+  expect(val.artists).toEqual(["Giselle", "Favored Nations"]);
+
+  val = inferTrack("WITCHER 3 SONG- Wake The White Wolf By Miracle Of Sound");
+  expect(val.title).toBe("Wake The White Wolf");
+  expect(val.artists).toEqual(["WITCHER 3 SONG", "Miracle Of Sound"]);
+
+  val = inferTrack(
+    "Miracle Of Sound - The Call - Elder Scrolls Online Song [pleer.com]"
+  );
+  expect(val.title).toBe("The Call");
+  expect(val.artists).toEqual(["Miracle Of Sound"]);
+  expect(val.album).toBe("Elder Scrolls Online Song");
+
+  val = inferTrack("Ambitiously Yours - S6E5 - New Friends and a Funeral");
+  expect(val.title).toBe("New Friends and a Funeral");
+  expect(val.artists).toEqual(["Ambitiously Yours"]);
+
+  val = inferTrack("Party in Peril: The Celestial Odyssey 06 (Oficial Audio)");
+  expect(val.title).toBe("The Celestial Odyssey 06");
+  expect(val.artists).toEqual(["Party in Peril"]);
+
+  val = inferTrack("Abandoned - Out Of The Grave (Feat. ENROSA) [NCS Release]");
+  expect(val.title).toBe("Out Of The Grave");
+  expect(val.artists).toEqual(["Abandoned", "ENROSA", "NCS"]);
+
+  val = inferTrack("Ali Sethi | Rung (Official Music Video)");
+  expect(val.title).toBe("Rung");
+  expect(val.artists).toEqual(["Ali Sethi"]);
+
+  val = inferTrack(
+    "Bruno Mars, Anderson .Paak, Silk Sonic - Leave the Door Open [Official Video]"
+  );
+  expect(val.title).toBe("Leave the Door Open");
+  expect(val.artists).toEqual(["Bruno Mars", "Anderson .Paak", "Silk Sonic"]);
+
+  val = inferTrack(
+    "Galasy ZMesta - Ya Nauchu Tebya (I'll Teach You) - Belarus - Official Video - Eurovision 2021"
+  );
+  expect(val.title).toBe("Ya Nauchu Tebya (I'll Teach You, Belarus)");
+  expect(val.year).toBe(2021);
+  expect(val.album).toBe("Eurovision");
+  expect(val.artists).toEqual(["Galasy ZMesta"]);
+
+  val = inferTrack(
+    "Game Of Thrones Theme (Music Box Vocal Version -- Cover of Karliene Lyrics)"
+  );
+  expect(val.title).toBe("Game Of Thrones Theme");
+  expect(val.artists).toEqual(["Music Box Vocal"]);
+
+  val = inferTrack(
+    "🎵MiatriSs🎵 - Yandere Song (The Original Song) [Русская Версия] + ENG Subtitles"
+  );
+  expect(val.title).toBe("Yandere Song (Русская Версия)");
+  expect(val.artists).toEqual(["MiatriSs"]);
+
+  val = inferTrack(
+    "【Helltaker Original Song】 What the Hell by @OR3O , @Lollia  , and @Sleeping Forest   ft. Friends"
+  );
+  expect(val.title).toBe("What the Hell");
+  expect(val.artists).toEqual(["OR3O", "Lollia", "Sleeping Forest", "Friends"]);
+
+  val = inferTrack("Splatoon ★ Blitz It (Remix\\Cover) | MiatriSs");
+  expect(val.title).toBe("Blitz It (Remix\\Cover)");
+  expect(val.artists).toEqual(["Splatoon"]);
+  expect(val.album).toEqual("MiatriSs");
+
+  val = inferTrack("ECHO【Gumi English】Crusher-P: MiatriSs Remix");
+  expect(val.title).toBe("Crusher-P (Gumi English)");
+  expect(val.artists).toEqual(["ECHO", "MiatriSs"]);
+
+  val = inferTrack(
+    "Endless Mistakes Cover - check out the original at https://soundcloud.com/"
+  );
+  expect(val.title).toBe("Endless Mistakes Cover");
+  expect(val.album).toBe("Endless Mistakes Cover");
+  expect(val.artists).toEqual(["Endless Mistakes"]);
+
+  val = inferTrack("Rihanna - The Monster (NO RAP/NO EMINEM) Edit +Lyrics");
+  expect(val.title).toBe("The Monster (NO RAP/NO EMINEM)");
+  expect(val.artists).toEqual(["Rihanna"]);
+
+  val = inferTrack(
+    "Fall - Ross Bugden ''Piano Tutorial'' (Piano Arrangement by AnubisMusic)"
+  );
+  expect(val.title).toBe("Ross Bugden");
+  expect(val.album).toBe("Ross Bugden");
+  expect(val.artists).toEqual(["Fall", "AnubisMusic"]);
+
+  val = inferTrack("Karliene. - Elizabeth");
+  expect(val.title).toBe("Elizabeth");
+  expect(val.artists).toEqual(["Karliene"]);
+
+  val = inferTrack(
+    "My Darkest Days - Porn Star Dancing (Rock Version) ft. Zakk Wylde (Official Video)"
+  );
+  expect(val.title).toBe("Porn Star Dancing (Rock Version)");
+  expect(val.album).toBe("Porn Star Dancing");
+  expect(val.artists).toEqual(["My Darkest Days", "Zakk Wylde"]);
+
+  val = inferTrack("Epic Trailer Music - Fall", true);
+  expect(val.title).toBe("Fall");
+  expect(val.album).toBe("Epic Trailer Music");
+  expect(val.artists).toEqual([]);
+
+  val = inferTrack("Epic Trailer Music - Fall - Nice Thing", true);
+  expect(val.title).toBe("Fall (Nice Thing)");
+  expect(val.album).toBe("Epic Trailer Music");
+  expect(val.artists).toEqual([]);
+
+  val = inferTrack(
+    "Epic and Dramatic Trailer Music - Olympus (Copyright and Royalty Free)",
+    true
+  );
+  expect(val.title).toBe("Olympus");
+  expect(val.album).toBe("Epic and Dramatic Trailer Music");
+  expect(val.artists).toEqual([]);
+
+  val = inferTrack('I:Scintilla - "Swimmers Can Drown" OFFICIAL VIDEO');
+  expect(val.title).toBe("Swimmers Can Drown");
+  expect(val.album).toBe("Swimmers Can Drown");
+  expect(val.artists).toEqual(["I:Scintilla"]);
+
+  val = inferTrack(
+    "The Pretty Reckless - Make Me Wanna Die (Acoustic Version)"
+  );
+  expect(val.title).toBe("Make Me Wanna Die (Acoustic Version)");
+  expect(val.album).toBe("Make Me Wanna Die");
+  expect(val.artists).toEqual(["The Pretty Reckless"]);
+
+  val = inferTrack("Rock - Rock Song - Under a Rock");
+  expect(val.title).toBe("Rock Song");
+  expect(val.album).toBe("Under a Rock");
+  expect(val.artists).toEqual(["Rock"]);
+
+  val = inferTrack("Nice song http://example.com");
+  expect(val.title).toBe("Nice song");
+});
+
+it("infers artists", () => {
+  expect(inferArtists("A & B")).toEqual(["A", "B"]);
+  expect(inferArtists("A,B ,C feat. D")).toEqual(["A", "B", "C", "D"]);
+  expect(inferArtists("A + 1")).toEqual(["A", "1"]);
+  expect(inferArtists("lol/KEK")).toEqual(["lol", "KEK"]);
+  expect(inferArtists("0///A+B,feat.D")).toEqual(["0", "A", "B", "D"]);
+  expect(inferArtists("Amedoand and ft Smb")).toEqual(["Amedoand", "Smb"]);
+});
---
+++ apps/core/data/infer.ts
@@ -0,0 +1,289 @@
+import genres from "./genres.json";
+const r = String.raw;
+
+const emoji = r`([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])`;
+const url =
+  /((?:(?:http?|ftp)[s]*:\/\/)?[a-z0-9-%/&=?.]+\.[a-z]{2,4}\/?([^\s<>#%",{}\\|\\^[\]`]+)?)/gi;
+const separators = [
+  emoji,
+  "\\|",
+  '"',
+  "`",
+  "''+",
+  ":\\s",
+  "\\s:",
+  "-\\s",
+  "\\s-",
+  "--+",
+  "(\\s|^)\\.+(\\s|$)",
+  "\\/\\/+",
+];
+
+function unbrace(text: string) {
+  const result = {
+    clean: "",
+    parts: [] as string[],
+  };
+
+  const opening = "[({【「";
+  const closing = "])}】」";
+  let current = -1;
+
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    const opened = opening.indexOf(char);
+    const closed = closing.indexOf(char);
+
+    if (opened >= 0) {
+      if (current >= 0) continue;
+      current = opened;
+      result.parts.push("");
+      continue;
+    }
+    if (current == -1) {
+      result.clean += char;
+      continue;
+    }
+    if (closed == current) {
+      const lastChar = result.clean[result.clean.length - 1];
+      if (lastChar) {
+        result.clean += " - ";
+      }
+      current = -1;
+      continue;
+    }
+    if (closed >= 0) continue;
+    result.parts[result.parts.length - 1] += char;
+  }
+
+  result.clean = trim(result.clean);
+  result.parts = result.parts.map((x) => x.trim()).filter((x) => x);
+
+  return result;
+}
+
+function unemojify(text: string) {
+  return text.replace(new RegExp(emoji, "ig"), "");
+}
+
+function split(text: string) {
+  const splits = new RegExp(separators.join("|"), "i");
+  return text.replace(url, "").split(splits);
+}
+
+function trim(text: string) {
+  const trim = /^[@'"`«»|—\-–/\\:\s.]+|['"`«»|—\-–/\\:\s.]+$/gi;
+  return text.replace(trim, "");
+}
+
+function isJunk(text: string) {
+  if (!text) return true;
+  if (text.match(url)) return true;
+
+  const junk = [
+    r`album`,
+    r`originals?`,
+    r`of+icial?`,
+    r`MV`,
+    r`lyrics?`,
+    r`video`,
+    r`live`,
+    r`only`,
+    r`mix`,
+    r`full`,
+    r`sub(titles?)?`,
+    r`quality`,
+    r`(HD|HQ|[0-9]{3,4}p|4Kb)`,
+    r`[0-9]{3,4}(p|bpmb)`,
+    r`S[0-9]+E[0-9]+`,
+    r`free`,
+    r`download`,
+    r`copyright`,
+    r`royalty`,
+    r`tutorial`,
+    r`amv`,
+    r`explicit`,
+    r`remaster(ed)?`,
+  ].map((x) => new RegExp(r`(\b|^|\s)${x}(\b|$|\s)`, "i"));
+
+  return junk.some((x) => text.match(x));
+}
+
+function isGenre(text: string, strict = false) {
+  text = text.toLowerCase();
+
+  if (strict) return genres.some((x) => x.toLowerCase() === text);
+  return genres.some((x) =>
+    text.match(new RegExp(r`(\s|^)${x.toLowerCase()}(\s|$)`))
+  );
+}
+
+function isJunkGenre(text: string) {
+  const filter = (x: string) => new RegExp(r`(\b|^)${x}(\b|$)`, "i");
+  const marks = [
+    "edit",
+    "rmx",
+    "remix",
+    "cover",
+    "(piano\\s+)?arrangement",
+    "edit?",
+    "version(\\s+cover)?",
+    "ver(\\s+cover)?",
+    "cover",
+    "release",
+  ];
+
+  return isGenre(text) && !marks.map(filter).find((x) => x.test(text));
+}
+
+function toYear(text: string) {
+  const regex = /(^|\b)([1-2][0-9]{3})(\b|$)/;
+  const match = text.match(regex);
+  if (!match) return null;
+  const year = +(match[0] || 0) || null;
+  if (!year) return null;
+  return [trim(text.replace(match[0], "")), year] as const;
+}
+
+function toArtist(text: string) {
+  const postfixes = [
+    "edit(ed)?",
+    "rmx",
+    "remix",
+    "version(\\s+cover)?",
+    "ver(\\s+cover)?",
+    "cover",
+    "dub",
+    "release",
+  ];
+  const prefixes = [
+    "edit(ed)?",
+    "rmx",
+    "remix(ed)?",
+    "cover(ed)?",
+    "performed",
+    "ft\\.",
+    "feat\\.?",
+    "featuring",
+    "med",
+    "(piano\\s+)?arrangement",
+    "by",
+  ];
+
+  const regexes = [];
+  regexes.push(
+    ...postfixes.map((x) => new RegExp(r`^(?<artist>.*?)\s+${x}(\b|$)`, "i"))
+  );
+  regexes.push(
+    ...prefixes.map(
+      (x) => new RegExp(r`(^|\b)${x}(\s+by)?\s+(?<artist>.*?)$`, "i")
+    )
+  );
+
+  const artists = [];
+  for (const regex of regexes) {
+    const match = text.match(regex);
+    const artist = match?.groups?.artist;
+    if (!match || !artist) continue;
+    if (isGenre(artist, true) || isJunk(artist)) continue;
+    artists.unshift(artist);
+    text = trim(text.replace(match[0], ""));
+  }
+
+  return [text, artists] as const;
+}
+
+function inferArtists(text?: string) {
+  if (!text) return [];
+  text = text.replace(/ - Topic$/i, "");
+  const joins = /,|\bft\.?|\bfeat\.?|&|\+|\/|\bfeaturing|\bmed\b|\band\b/i;
+
+  return [
+    ...new Set(
+      text
+        .split(joins)
+        .map((x) => trim(x))
+        .filter((x) => x)
+    ),
+  ];
+}
+
+function inferTrack(text: string, artistless = false) {
+  const { parts, clean } = unbrace(text);
+  let atoms = [...split(clean), ...parts].filter((x) => x);
+  atoms = [...new Set(atoms)];
+  atoms = atoms.map((x) => trim(x)).filter((x) => x);
+
+  let title = clean;
+  let album = "";
+  let year = undefined as number | undefined;
+  let artists = [];
+  let meta = [] as string[];
+
+  atoms = atoms.map((x) => {
+    x = unemojify(x);
+    const tryYear = toYear(x);
+    if (tryYear) {
+      x = tryYear[0];
+      year = tryYear[1];
+      if (!x) return "";
+    }
+
+    const parsedArtists = toArtist(x);
+    x = parsedArtists[0];
+    for (const artist of parsedArtists[1]) {
+      artists.push(...inferArtists(artist));
+    }
+
+    if (isJunk(x)) {
+      return "";
+    }
+
+    if (parts.includes(x)) {
+      meta.push(x);
+      return "";
+    }
+
+    return x;
+  });
+  atoms = atoms.filter((x) => x);
+
+  switch (atoms.length) {
+    case 0:
+      title = [...split(clean)].filter((x) => !isJunk(x)).join("; ");
+      if (!title) title = clean;
+      if (!title) title = parts.join("; ");
+      if (!title) title = text;
+      break;
+    case 1:
+      title = atoms[0];
+      break;
+    case 2:
+      if (artistless) album = atoms[0];
+      else artists.unshift(...inferArtists(atoms[0]));
+      title = atoms[1];
+      break;
+    default:
+      if (artistless) album = atoms[0];
+      else artists.unshift(...inferArtists(atoms[0]));
+      title = atoms[1];
+      meta.push(...atoms.slice(2, atoms.length - 1 * +!artistless));
+      if (!artistless) album = atoms[atoms.length - 1];
+      break;
+  }
+
+  meta = meta.filter((x) => !isJunkGenre(x));
+  artists = artists.map((x) => trim(x));
+
+  album = album || title;
+  if (meta.length) title += ` (${meta.join(", ")})`;
+
+  return {
+    title,
+    artists,
+    album,
+    year,
+  };
+}
+
+export { inferTrack, inferArtists };
