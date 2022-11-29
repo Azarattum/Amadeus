07ae87b999581780742bd5554611c0c123ce2b03
Azarattum <azarattum@pm.me>
2022-11-29 15:39:06 +0700

    Moved genres.json to assets

--- apps/core/data/infer.ts
+++ apps/core/data/infer.ts
@@ -1,4 +1,4 @@
-import genres from "./genres.json";
+import genres from "../assets/genres.json";
 const r = String.raw;
 
 const emoji = r`([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])`;
