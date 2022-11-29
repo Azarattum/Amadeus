d4f9be66ba4ed09257b7ecd0316cdb9089978e67
Azarattum <azarattum@pm.me>
2022-11-29 19:15:50 +0700

    Fixed svelte prettier formatting

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
+  pluginSearchDirs: false,
   plugins: [
+    require("prettier-plugin-svelte"),
     require("prettier-plugin-tailwindcss"),
     require("prettier-plugin-sort-imports"),
   ],
