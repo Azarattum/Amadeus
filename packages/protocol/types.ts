e86e4023aec1d26223ccab55557afdeb3e75191b
Azarattum <azarattum@pm.me>
2023-01-11 14:30:17 +0700

    Renamed protocol types

---
+++ packages/protocol/types.ts
@@ -0,0 +1,57 @@
+import {
+  object,
+  number,
+  string,
+  array,
+  type Infer,
+  intersection,
+  Struct,
+  union,
+  omit,
+  nullable,
+} from "superstruct";
+import type { ObjectSchema, ObjectType } from "superstruct/dist/utils";
+
+type Track = Infer<typeof track>;
+const track = object({
+  title: string(),
+  artists: array(string()),
+  album: string(),
+  year: nullable(number()),
+  source: array(string()),
+  art: array(string()),
+  length: number(),
+});
+
+type Artist = Infer<typeof artist>;
+const artist = object({
+  title: string(),
+  source: array(string()),
+  art: array(string()),
+});
+
+type Album = Infer<typeof album>;
+const album = object({
+  title: string(),
+  source: array(string()),
+  art: array(string()),
+});
+
+type Unique<T> = T & { id: string };
+const unique = <T extends Struct<any>>(t: T) =>
+  intersection([t, object({ id: string() })]);
+
+type Abstract<T> = T extends any ? Omit<T, "source" | "art" | "length"> : never;
+const abstract = <T extends ObjectSchema>(t: Struct<ObjectType<T>, T>) =>
+  omit(t, ["source", "art", "length"]);
+
+type Media = Infer<typeof media>;
+const media = union([track, artist, album]);
+
+type Source = Infer<typeof source>;
+const source = object({
+  url: string(),
+});
+
+export { media, track, artist, album, source, unique, abstract };
+export type { Media, Track, Artist, Album, Source, Unique, Abstract };
