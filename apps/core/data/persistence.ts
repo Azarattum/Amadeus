8e0d4a845da749b073f0e9c37bbbce71dab25522
Azarattum <azarattum@pm.me>
2023-02-15 18:21:15 +0700

    Implemented plugin specific dynamic data tables

--- apps/core/data/persistence.ts
+++ apps/core/data/persistence.ts
@@ -4,6 +4,7 @@ import { database } from "../event/pool";
 import { take } from "libfun";
 
 function persistence(this: Context, user?: string) {
+  const group = this?.group || "core";
   const shared = take(database.bind(this)());
   const connections = take(database.bind(this)(user));
 
@@ -11,26 +12,47 @@ function persistence(this: Context, user?: string) {
     async push(tracks: TrackDetails[], playlist?: number) {
       const [dbs, cache] = await Promise.all([connections, shared]);
       await Promise.all([
-        ...(user ? dbs.map((x) => x.push(tracks, playlist)) : []),
-        ...cache.map((x) => x.push(tracks)),
+        ...(user ? dbs.map((x) => x.push?.(tracks, playlist)) : []),
+        ...cache.map((x) => x.push?.(tracks)),
       ]);
     },
     async purge(entries: number[]) {
       const dbs = await connections;
-      await Promise.all(dbs.map((x) => x.purge(entries)));
+      await Promise.all(dbs.map((x) => x.purge?.(entries)));
+    },
+    async store(key: string, value: string) {
+      const dbs = await connections;
+      await Promise.all(dbs.map((x) => x.store?.(group, key, value)));
+    },
+    async lookup(value: string) {
+      const dbs = await connections;
+      return Promise.race(
+        dbs.map((x) => x.lookup?.(group, value)).filter((x) => x)
+      );
+    },
+    async extract(key: string) {
+      const dbs = await connections;
+      return Promise.race(
+        dbs.map((x) => x.extract?.(group, key)).filter((x) => x)
+      );
     },
     async create(playlist: string) {
       const dbs = await connections;
-      await Promise.all(dbs.map((x) => x.create(playlist)));
+      await Promise.all(dbs.map((x) => x.create?.(playlist)));
     },
   };
 }
 
-type Database = {
-  push(tracks: TrackDetails[], playlist?: number): void | Promise<void>;
-  purge(entries: number[]): void | Promise<void>;
-  create(name: string): void | Promise<void>;
-};
+type Database = Partial<{
+  push(tracks: TrackDetails[], playlist?: number): Promise<void>;
+  purge(entries: number[]): Promise<void>;
+
+  store(collection: string, key: string, value: string): Promise<void>;
+  lookup(collection: string, value: string): Promise<string>;
+  extract(collection: string, key: string): Promise<string>;
+
+  create(name: string): Promise<void>;
+}>;
 
 export type { Database };
 export { persistence };
--- plugins/sqlite/index.ts
+++ plugins/sqlite/index.ts
@@ -5,7 +5,47 @@ import { async } from "@amadeus-music/core";
 
 database(function* (user = "shared") {
   if (!existsSync("users")) mkdirSync("users");
-  yield connect(`users/${user}.db`, user === "shared").playlists;
+  const db = connect(`users/${user}.db`, user === "shared");
+  yield {
+    ...db.playlists,
+    async store(collection, key, value) {
+      return db.update(async (db) => {
+        await db.schema
+          .createTable(collection)
+          .ifNotExists()
+          .addColumn("key", "text", (x) => x.primaryKey())
+          .addColumn("value", "text")
+          .execute();
+        await db.schema
+          .createIndex(collection + "_value")
+          .ifNotExists()
+          .on(collection)
+          .column("value")
+          .execute();
+        await db
+          .insertInto(collection as any)
+          .onConflict((x) => x.doUpdateSet({ value }))
+          .values({ key, value })
+          .execute();
+      });
+    },
+    async extract(collection, key) {
+      return (await db.connection)
+        .selectFrom(collection as any)
+        .select("value")
+        .where("key", "=", key)
+        .executeTakeFirstOrThrow()
+        .then((x) => x.value);
+    },
+    async lookup(collection, value) {
+      return (await db.connection)
+        .selectFrom(collection as any)
+        .select("key")
+        .where("value", "=", value)
+        .executeTakeFirstOrThrow()
+        .then((x) => x.key);
+    },
+  };
 });
 
 stop(function* () {
