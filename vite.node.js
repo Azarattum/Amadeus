85775d816c9b5a98d329563f18b3dc726f9f37f3
Azarattum <azarattum@pm.me>
2023-03-01 18:00:59 +0700

    Upgraded dependencies

--- vite.node.js
+++ vite.node.js
@@ -3,7 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [{ ...externals({ deps: false }), enforce: "pre" }],
-  conditions: ["import", "module", "node", "default"],
+  resolve: {
+    conditions: ["import", "module", "node", "default"],
+  },
   test: {
     exclude: ["node_modules", "build", ".svelte-kit"],
     coverage: {
