ab1a590719437be25c54acfb6656325313f96d63
Azarattum <azarattum@pm.me>
2023-03-06 19:16:41 +0700

    Updated eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,7 @@ module.exports = {
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "spaced-comment": ["warn", "always", { markers: ["/"] }],
     "@typescript-eslint/ban-types": [
       "error",
