ff77aa53444de2199322f09c340f6e25bd0d588a
Azarattum <azarattum@pm.me>
2023-03-10 11:19:39 +0700

    Implemented artist follow actions

--- packages/crdata/stores/artists.ts
+++ packages/crdata/stores/artists.ts
@@ -6,6 +6,16 @@ export const artists = ({ store }: DB) =>
     async edit(db, id: number, artist: Partial<Artist>) {
       await db.updateTable("artists").where("id", "=", id).set(artist);
     },
+    async follow(db, id: number) {
+      await db
+        .insertInto("following")
+        .onConflict((x) => x.doNothing())
+        .values({ artist: id, seen: null })
+        .execute();
+    },
+    async unfollow(db, id: number) {
+      await db.deleteFrom("following").where("artist", "=", id).execute();
+    },
     get(db, id: number) {
       return db
         .selectFrom("artists")
