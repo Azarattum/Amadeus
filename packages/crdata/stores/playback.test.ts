c56643ae633f01ea59f905f308f2a5b604c968f6
Azarattum <azarattum@pm.me>
2023-03-30 16:35:58 +0700

    Reimplemented playback with new view and triggers

--- packages/crdata/stores/playback.test.ts
+++ packages/crdata/stores/playback.test.ts
@@ -3,7 +3,7 @@ import { TrackDetails } from "@amadeus-music/protocol";
 import { connect } from "../data";
 import { rm } from "fs/promises";
 
-const { playback, preceding, playing, upcoming, close } = connect({
+const { playback, preceding, upcoming, close, feed } = connect({
   name: "test.db",
 });
 
@@ -23,61 +23,151 @@ const t3 = track(3);
 const t4 = track(4);
 const t5 = track(5);
 
-it("pushes tracks", async () => {
+it("pushes with direction", async () => {
   expect(await preceding).toHaveLength(0);
-  expect(await playing).toHaveLength(0);
+  expect(await playback).toHaveLength(0);
   expect(await upcoming).toHaveLength(0);
 
   await playback.push([t0], "last");
   expect(await preceding).toHaveLength(0);
-  expect(await playing).toMatchObject([t0]);
+  expect(await playback).toMatchObject([t0]);
   expect(await upcoming).toHaveLength(0);
 
   await playback.push([t1], "last");
   expect(await preceding).toHaveLength(0);
-  expect(await playing).toMatchObject([t0]);
+  expect(await playback).toMatchObject([t0]);
   expect(await upcoming).toMatchObject([t1]);
 
   await playback.push([t2, t3], "last");
   expect(await preceding).toHaveLength(0);
-  expect(await playing).toMatchObject([t0]);
+  expect(await playback).toMatchObject([t0]);
   expect(await upcoming).toMatchObject([t1, t2, t3]);
 
   await playback.push([t4], "next");
   expect(await preceding).toHaveLength(0);
-  expect(await playing).toMatchObject([t0]);
+  expect(await playback).toMatchObject([t0]);
   expect(await upcoming).toMatchObject([t4, t1, t2, t3]);
 
-  await playback.push([t5], "awaiting");
-  expect(await preceding).toHaveLength(0);
-  expect(await playing).toMatchObject([t0]);
+  await playback.push([t5, t0], "first");
+  expect(await preceding).toMatchObject([t5, t0]);
+  expect(await playback).toMatchObject([t0]);
   expect(await upcoming).toMatchObject([t4, t1, t2, t3]);
+
+  await playback.redirect("backward");
+  expect(await preceding).toMatchObject([t3, t2, t1, t4]);
+  expect(await playback).toMatchObject([t0]);
+  expect(await upcoming).toMatchObject([t0, t5]);
+
+  await playback.push([t0, t1], "first");
+  expect(await preceding).toMatchObject([t0, t1, t3, t2, t1, t4]);
+  expect(await playback).toMatchObject([t0]);
+  expect(await upcoming).toMatchObject([t0, t5]);
+
+  await playback.push([t2, t3], "next");
+  expect(await preceding).toMatchObject([t0, t1, t3, t2, t1, t4]);
+  expect(await playback).toMatchObject([t0]);
+  expect(await upcoming).toMatchObject([t2, t3, t0, t5]);
+
+  await playback.push([t4, t5], "last");
+  expect(await preceding).toMatchObject([t0, t1, t3, t2, t1, t4]);
+  expect(await playback).toMatchObject([t0]);
+  expect(await upcoming).toMatchObject([t2, t3, t0, t5, t4, t5]);
+
+  await playback.redirect("forward");
+  expect(await preceding).toMatchObject([t5, t4, t5, t0, t3, t2]);
+  expect(await playback).toMatchObject([t0]);
+  expect(await upcoming).toMatchObject([t4, t1, t2, t3, t1, t0]);
+});
+
+it("pushes after index", async () => {
+  await playback.push([t0, t1, t2]);
+  const [{ entry: current }] = await playback;
+  const [{ entry: next }] = await upcoming;
+  expect(next).toBeTypeOf("number");
+
+  expect(await upcoming).toMatchObject([t1, t2]);
+  await playback.push([t3, t4], next);
+  expect(await upcoming).toMatchObject([t1, t3, t4, t2]);
+  await playback.push([t5, t0], current);
+  expect(await upcoming).toMatchObject([t5, t0, t1, t3, t4, t2]);
+  await playback.sync(1);
+  await playback.sync(1);
+  await playback.redirect("backward");
+  expect(await upcoming).toMatchObject([t5, t0]);
+  const [{ entry: after2 }] = await preceding;
+  const [{ entry: after0 }] = await playback;
+  const [{ entry: after5 }] = await upcoming;
+  await playback.push([t3, t4], after5);
+  expect(await upcoming).toMatchObject([t5, t3, t4, t0]);
+  await playback.push([t1, t2], after0);
+  expect(await upcoming).toMatchObject([t1, t2, t5, t3, t4, t0]);
+  await playback.push([t5, t0], after2);
+  expect(await preceding).toMatchObject([t2, t5, t0, t4, t3, t1]);
+});
+
+it("pushes randomly", async () => {
+  await playback.push([t2, t3, t4, t5]);
+  await playback.push([t0, t1], "first");
+  expect(await preceding).toMatchObject([t0, t1]);
+  expect(await playback).toMatchObject([t2]);
+  expect(await upcoming).toMatchObject([t3, t4, t5]);
+
+  await playback.redirect("shuffled");
+  expect(await preceding).toMatchObject([]);
+  expect(await playback).toMatchObject([t2]);
+  const tracks = await upcoming;
+  const expected = [t0, t1, t3, t4, t5].map((t) => expect.objectContaining(t));
+  expect(tracks).toEqual(expect.arrayContaining(expected));
+  expect(tracks).not.toEqual(
+    expect.arrayContaining([expect.objectContaining(t2)])
+  );
+
+  const next = tracks[0];
+  await playback.sync(1);
+  expect(await preceding).toMatchObject([t2]);
+  expect(await playback).toMatchObject([next]);
+  expect(await upcoming).not.toEqual(
+    expect.arrayContaining([expect.objectContaining(next)])
+  );
+
+  await playback.redirect("forward");
+  (await preceding).forEach((x) => expect(x.id < next.id));
+  expect(await playback).toMatchObject([next]);
+  (await upcoming).forEach((x) => expect(x.id > next.id));
 });
 
 it("purges tracks", async () => {
-  await playback.push([t0, t1, t2], "next");
-  const [{ entry: current }] = await playing;
+  await playback.push([t0, t1, t2]);
+  const [{ entry: current }] = await playback;
   const [{ entry: next }] = await upcoming;
   expect(next).toBeTypeOf("number");
+  await playback.sync(0.5);
   await playback.purge([current, next as number]);
 
-  expect(await playing).toMatchObject([t2]);
+  expect(await playback).toMatchObject([{ ...t2, progress: 0 }]);
   expect(await upcoming).toHaveLength(0);
 });
 
-it("syncs & backtracks", async () => {
+it("syncs progress", async () => {
   await playback.push([t0, t1], "next");
-  expect(await playing).toMatchObject([t0]);
+  expect(await playback).toMatchObject([t0]);
   await playback.sync(0.3);
-  expect(await playing).toMatchObject([{ ...t0, state: 0.3 }]);
+  expect(await playback).toMatchObject([{ ...t0, progress: 0.3 }]);
   await playback.sync(1);
-  expect(await playing).toMatchObject([t1]);
+  expect(await playback).toMatchObject([{ ...t1, progress: 0 }]);
   expect(await preceding).toMatchObject([t0]);
+  await playback.sync(1);
+  expect(await playback).toMatchObject([t1]);
+  await playback.sync(0.5);
 
-  await playback.backtrack();
+  await playback.sync(-1);
   expect(await preceding).toHaveLength(0);
-  expect(await playing).toMatchObject([t0]);
+  expect(await playback).toMatchObject([{ ...t0, progress: 0 }]);
   expect(await upcoming).toMatchObject([t1]);
+  await playback.sync(-1);
+  expect(await playback).toMatchObject([t0]);
+
+  expect((await feed)[0].tracks).toMatchObject([t1, t0]);
 });
 
 it("skips with repeat all", async () => {
@@ -86,12 +176,12 @@ it("skips with repeat all", async () => {
   await playback.sync(1);
   await playback.sync(1);
 
-  expect(await preceding).toMatchObject([t1, t0]);
-  expect(await playing).toMatchObject([t2]);
+  expect(await preceding).toMatchObject([t0, t1]);
+  expect(await playback).toMatchObject([t2]);
   expect(await upcoming).toHaveLength(0);
   await playback.sync(1);
   expect(await preceding).toHaveLength(0);
-  expect(await playing).toMatchObject([t0]);
+  expect(await playback).toMatchObject([t0]);
   expect(await upcoming).toMatchObject([t1, t2]);
 });
 
@@ -111,14 +201,10 @@ it("clears tracks", async () => {
   await playback.push([t0, t1]);
   await playback.sync(1);
 
-  expect(await playing).toMatchObject([t1]);
+  expect(await playback).toMatchObject([t1]);
   expect(await preceding).toMatchObject([t0]);
-
-  await playback.clear(true);
-  expect(await playing).toMatchObject([t1]);
-  expect(await preceding).toHaveLength(0);
   await playback.clear();
-  expect(await playing).toHaveLength(0);
+  expect(await playback).toHaveLength(0);
 });
 
 afterAll(async () => {
@@ -130,4 +216,5 @@ afterEach(async () => {
   await playback.clear();
   await playback.repeat("none");
   await playback.redirect("forward");
+  await feed.clear("listened");
 });
--- packages/crdata/stores/playback.ts
+++ packages/crdata/stores/playback.ts
@@ -1,220 +1,119 @@
-import type {
-  TrackDetails,
-  PlaybackRepeat,
-  PlaybackDirection,
-  PlaybackPosition,
-} from "@amadeus-music/protocol";
 import {
   metadata,
-  current,
   push,
   uuid,
   localDevice,
   metafields,
+  position,
 } from "../data/operations";
+import type {
+  TrackDetails,
+  PlaybackRepeat,
+  PlaybackDirection,
+} from "@amadeus-music/protocol";
 import type { DB } from "../data/schema";
-import { APPEND, sql } from "crstore";
+import { sql } from "crstore";
 
 export const preceding = ({ store }: DB) =>
   store((db) =>
     db
       .with("metadata", metadata)
-      .selectFrom("playback")
-      .innerJoin("metadata", "metadata.id", "playback.track")
-      .where("device", "=", localDevice)
-      .where("state", ">=", 1)
-      .orderBy("order", "desc")
-      .orderBy("playback.id", "desc")
-      .select("playback.id as entry")
+      .selectFrom("queue")
+      .innerJoin("metadata", "metadata.id", "queue.track")
+      .where("position", "<", 0)
+      .select("queue.id as entry")
       .select(metafields)
-  );
-
-export const playing = ({ store }: DB) =>
-  store((db) =>
-    db
-      .with("metadata", metadata)
-      .selectFrom((qb) =>
-        qb
-          .selectFrom("playback")
-          .innerJoin("metadata", "metadata.id", "playback.track")
-          .where("state", "<", 1)
-          .select(["device", "state"])
-          .select("playback.id as entry")
-          .select(sql<boolean>`device = crsql_siteid()`.as("local"))
-          .select(metafields)
-          .orderBy("state", "desc")
-          .orderBy("order")
-          .orderBy("playback.id")
-          .as("data")
-      )
-      .selectAll()
-      .groupBy("device")
   );
 
 export const upcoming = ({ store }: DB) =>
   store((db) =>
     db
       .with("metadata", metadata)
-      .with("current", current)
-      .selectFrom("devices")
-      .where("devices.id", "=", localDevice)
-      .innerJoin("playback", "playback.device", "devices.id")
-      .innerJoin("metadata", "metadata.id", "playback.track")
-      .where("state", "<", 1)
-      .where((qb) =>
-        qb
-          .where((x) => x.where("state", ">=", 0).where("direction", "=", 0))
-          .orWhere((x) => x.where("state", "<", 0).where("direction", "=", 1))
-          .orWhere("direction", "=", 2)
-          .orWhere("repeat", "=", 2)
-      )
-      .whereRef(
-        (qb) => qb.selectFrom("current").select("id"),
-        "!=",
-        "playback.id"
-      )
-      .select("playback.id as entry")
-      .select(metafields).orderBy(sql`
-        CASE WHEN "direction" = 0 THEN "state" ELSE NULL END DESC,
-        CASE WHEN "direction" = 1 THEN "state" ELSE NULL END ASC,
-        CASE WHEN "direction" != 1 THEN "order" ELSE NULL END ASC,
-        CASE WHEN "direction" = 1 THEN "order" ELSE NULL END DESC,
-        "playback"."id"
-      `)
+      .selectFrom("queue")
+      .innerJoin("metadata", "metadata.id", "queue.track")
+      .where("position", ">", 0)
+      .select("queue.id as entry")
+      .select(metafields)
   );
 
 export const playback = ({ store }: DB) =>
   store(
-    (db) => db.selectFrom("devices").where("id", "=", localDevice).selectAll(),
+    (db) =>
+      db
+        .with("metadata", metadata)
+        .selectFrom("devices")
+        .innerJoin("playback", "playback.id", "devices.playback")
+        .innerJoin("metadata", "metadata.id", "playback.track")
+        .select(["direction", "repeat", "infinite", "device", "progress"])
+        .select("playback.id as entry")
+        .select(sql<boolean>`device = crsql_siteid()`.as("local"))
+        .select(metafields),
     {
       async push(
         db,
         tracks: TrackDetails[],
-        position: PlaybackPosition = "next"
+        at: "first" | "next" | "last" | "random" | number = "next"
       ) {
         await Promise.all(tracks.map((x) => push(db, x)));
+        const { direction } = await db
+          .selectFrom("devices")
+          .where("id", "=", localDevice)
+          .select("direction")
+          .executeTakeFirstOrThrow();
 
         const ids = tracks.map(uuid);
-        if (position === "last") {
-          await db
-            .insertInto("playback")
-            .values(
-              tracks.map((track, i) => ({
-                id: ids[i],
-                device: localDevice,
-                track: track.id,
-                /// Support all directions
-                state: 0,
-                order: APPEND,
-                temp: null,
-              }))
-            )
-            .execute();
-        } else if (position === "next") {
-          await db
-            .insertInto("playback_fractindex" as any)
-            .values(
-              tracks.map((track, i) => ({
-                id: ids[i],
-                device: localDevice,
-                track: track.id,
-                /// Support all directions
-                state: 0,
-                after_id:
-                  i > 0
-                    ? ids[i - 1]
-                    : (x: typeof db) => current(x).clearSelect().select("id"),
-              }))
-            )
-            .execute();
-        }
-        if (Number.isInteger(position)) {
-          /// TODO
-        }
+        const order =
+          direction != 1
+            ? Number.isInteger(at)
+              ? at
+              : at === "first"
+              ? position.first
+              : at === "next"
+              ? position.next
+              : position.last
+            : Number.isInteger(at)
+            ? position.shift(+at)
+            : at === "first"
+            ? position.last
+            : at === "next"
+            ? position.before
+            : position.first;
+
+        /// TODO: support random
+        await db
+          .insertInto("playback_fractindex" as any)
+          .values(
+            tracks.map((track, i) => ({
+              id: ids[i],
+              track: track.id,
+              device: localDevice,
+              after_id: i > 0 && direction != 1 ? ids[i - 1] : order,
+            }))
+          )
+          .execute();
       },
       async purge(db, entries: number[]) {
         await db.deleteFrom("playback").where("id", "in", entries).execute();
       },
-      async clear(db, onlyHistory?: boolean, device = localDevice) {
-        if (onlyHistory) {
-          await db
-            .deleteFrom("playback")
-            .where("device", "=", device)
-            .where("state", "=", 1)
-            .execute();
-        } else {
-          await db
-            .deleteFrom("playback")
-            .where("device", "=", device)
-            .execute();
-        }
+      async clear(db, device: Uint8Array = localDevice as any) {
+        await db.deleteFrom("playback").where("device", "=", device).execute();
+        await db
+          .updateTable("devices")
+          .set({ playback: null, progress: 0 })
+          .where("id", "=", device)
+          .execute();
       },
       async sync(db, progress: number) {
         await db
-          .with("current", current)
-          .updateTable("playback")
-          .whereRef(
-            (qb) => qb.selectFrom("current").select("id"),
-            "=",
-            "playback.id"
-          )
-          .set({ state: progress })
-          .execute();
-        if (progress != 1) return;
-        // Track is complete, check what's left
-        const left = await db
-          .selectFrom("devices")
+          .updateTable("devices")
           .where("id", "=", localDevice)
-          .where("repeat", "=", 2)
-          .select([
-            (qb) =>
-              qb
-                .selectFrom("playback")
-                .where("device", "=", localDevice)
-                .select((eb) => eb.fn.count("id").as("count"))
-                .where("state", "!=", 1)
-                .$castTo<{ count: number }>()
-                .as("count"),
-          ])
-          .limit(1)
-          .executeTakeFirst();
-        if (!left || left.count) return;
-        // No tracks left, repeat all is enabled
-        await db
-          .updateTable("playback")
-          .where("device", "=", localDevice)
-          .set({ state: 0 })
-          .execute();
-      },
-      async backtrack(db) {
-        await db
-          .with("current", current)
-          .updateTable("playback")
-          .whereRef(
-            (qb) => qb.selectFrom("current").select("id"),
-            "=",
-            "playback.id"
-          )
-          .orWhereRef(
-            (qb) =>
-              qb
-                .selectFrom("playback")
-                .where("device", "=", localDevice)
-                .where("state", "=", 1)
-                .orderBy("order", "desc")
-                .orderBy("id", "desc")
-                .select("id")
-                .limit(1),
-            "=",
-            "playback.id"
-          )
-          .set({ state: 0 })
+          .set({ progress })
           .execute();
       },
       async rearrange(db, track: number, after?: number) {
         await db
           .updateTable("playback_fractindex" as any)
-          .set({ after_id: after })
+          .set({ after_id: after }) /// TODO: support other directions
           .where("id", "=", track)
           .execute();
       },
@@ -232,30 +131,6 @@ export const playback = ({ store }: DB) =>
           .set({ direction: to })
           .where("id", "=", localDevice)
           .execute();
-        if (to === directions.indexOf("shuffled")) {
-          await db
-            .with("current", current)
-            .updateTable("playback")
-            .where("device", "=", localDevice)
-            .where("state", "<", 1)
-            .whereRef(
-              (qb) => qb.selectFrom("current").select("id"),
-              "!=",
-              "playback.id"
-            )
-            .set({
-              order: sql`(SELECT "order" FROM current)||hex(randomblob(2))`,
-              temp: (qb: any) => qb.ref("order"),
-            })
-            .execute();
-        } else if (from === directions.indexOf("shuffled")) {
-          await db
-            .updateTable("playback")
-            .where("device", "=", localDevice)
-            .where("temp", "is not", null)
-            .set({ order: (qb) => qb.ref("temp"), temp: null })
-            .execute();
-        }
       },
       async repeat(db, type: PlaybackRepeat) {
         const types = ["none", "single", "all"];
@@ -283,7 +158,7 @@ export const playback = ({ store }: DB) =>
             qb
               .selectFrom("playback")
               .select(sql`${localDevice}`.as("device"))
-              .select(["id", "order", "temp", "track", "state"])
+              .select(["id", "order", "temp", "track"])
               .where("device", "=", device)
           )
           .execute();
@@ -294,7 +169,13 @@ export const playback = ({ store }: DB) =>
             qb
               .selectFrom("devices")
               .select(sql`${localDevice}`.as("id"))
-              .select(["direction", "repeat", "infinite"])
+              .select([
+                "direction",
+                "repeat",
+                "infinite",
+                "playback",
+                "progress",
+              ])
               .where("id", "=", device)
           )
           .execute();
