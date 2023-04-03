c950b5a16f93ff5b40d3405cff0b77d18d113c0d
Azarattum <azarattum@pm.me>
2023-04-04 02:09:26 +0700

    Prototyped navbar

---
+++ packages/ui/lib/elements/Nav.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Header, Logo, VStack } from "@amadeus-music/ui";
+
+  export let section: string;
+</script>
+
+<div
+  class="outline-highlight-100 flex min-h-screen w-max flex-col outline outline-1"
+>
+  <Logo />
+  <nav
+    class="border-highlight-100 flex h-full flex-col items-center justify-between border-t p-4"
+  >
+    <VStack gap="sm">
+      <Header>{section}</Header>
+      <slot />
+    </VStack>
+
+    <slot name="bottom" />
+  </nav>
+</div>
