6b5ecaaa1ca797ff309a2f1955b5f38882b78bc2
Azarattum <azarattum@pm.me>
2023-04-04 21:09:41 +0700

    Extended stack components

--- packages/ui/lib/layout/VStack.svelte
+++ packages/ui/lib/layout/VStack.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+  import { setContext } from "svelte";
+  setContext("stack", "vertical");
+
   export let gap: boolean | "sm" | "lg" | "xl" = false;
+  export let center = false;
+  export let grow = false;
+  export let p = false;
 </script>
 
 <div
-  class="flex w-full flex-col"
+  class="flex flex-col"
+  class:items-center={center}
   class:gap-1={gap === "sm"}
   class:gap-2={gap === true}
   class:gap-4={gap === "lg"}
   class:gap-8={gap === "xl"}
+  class:p-4={p === true}
+  class:flex-grow={grow}
 >
   <slot />
 </div>
