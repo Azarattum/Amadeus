9c8599b6801a2ec7ebaee94c461ff33e25224333
Azarattum <azarattum@pm.me>
2023-04-04 21:06:23 +0700

    Added spacer and separator layout components

--- packages/ui/component.ts
+++ packages/ui/component.ts
@@ -5,10 +5,12 @@ import "./lib/tailwind.pcss";
 export { default as Portal } from "./lib/layout/Portal.svelte";
 export { default as VStack } from "./lib/layout/VStack.svelte";
 export { default as HStack } from "./lib/layout/HStack.svelte";
+export { default as Spacer } from "./lib/layout/Spacer.svelte";
 export { default as Gateway } from "./lib/layout/Gateway.svelte";
 export { default as Overlay } from "./lib/layout/Overlay.svelte";
 export { default as Virtual } from "./lib/layout/Virtual.svelte";
 export { default as Sortable } from "./lib/layout/Sortable.svelte";
+export { default as Separator } from "./lib/layout/Separator.svelte";
 
 export { default as Logo } from "./lib/primitive/Logo.svelte";
 export { default as Icon } from "./lib/primitive/Icon.svelte";
---
+++ packages/ui/lib/layout/Separator.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+
+  export let vertical = getContext("stack") === "horizontal";
+</script>
+
+<hr class="border-highlight {vertical ? 'h-full w-0 border-r' : 'w-full'}" />
