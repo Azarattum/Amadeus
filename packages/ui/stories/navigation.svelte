656a6e90a04b2f83ac67349bd6823108079cb8df
Azarattum <azarattum@pm.me>
2023-04-04 22:07:45 +0700

    Added a navigation story

---
+++ packages/ui/stories/navigation.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { Nav, Button, Icon, HStack } from "@amadeus-music/ui";
+
+  const icons: Record<string, string> = {
+    Home: "house",
+    Library: "note",
+    Explore: "compass",
+    Added: "last",
+    Playlists: "queue",
+    Artists: "artists",
+    Timeline: "clock",
+    Tracks: "note",
+    Albums: "album",
+    Listened: "history",
+    Recommended: "stars",
+    Following: "artists",
+  };
+
+  const sections: Record<string, string[]> = {
+    Home: ["Added", "Listened", "Recommended", "Following"],
+    Library: ["Playlists", "Artists", "Timeline"],
+    Explore: ["Tracks", "Artists", "Albums"],
+  };
+
+  let section = "Home";
+  let subsection = "Listened";
+  $: subsection = sections[section][0];
+</script>
+
+<HStack gap="xl">
+  <Nav {section}>
+    {#each Object.keys(sections) as x}
+      <Button
+        air
+        compact
+        stretch
+        active={x === section}
+        on:click={() => (section = x)}
+      >
+        <Icon md name={icons[x]} />{x}
+      </Button>
+    {/each}
+    <svelte:fragment slot="section">
+      {#each sections[section] as x}
+        <Button air active={x === subsection} on:click={() => (subsection = x)}>
+          <Icon name={icons[x]} />{x}
+        </Button>
+      {/each}
+    </svelte:fragment>
+  </Nav>
+</HStack>
