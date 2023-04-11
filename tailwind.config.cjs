9519a0234bc3154b06d67910fbae2dc3f9479836
Azarattum <azarattum@pm.me>
2023-04-11 14:16:45 +0700

    Added plugin to improve hover support on mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,5 +31,17 @@ module.exports = {
         ),
       ]);
     }),
+    plugin(({ addVariant, e }) => {
+      addVariant("hover", [
+        ({ modifySelectors }) => (
+          modifySelectors(({ className }) => `.hover\\:${e(className)}:hover`),
+          "@media (hover: hover) and (pointer: fine)"
+        ),
+        ({ modifySelectors }) => (
+          modifySelectors(({ className }) => `.hover\\:${e(className)}:active`),
+          "@media (pointer: coarse)"
+        ),
+      ]);
+    }),
   ],
 };
