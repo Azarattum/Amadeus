9f17905d1ac3f7b385059fa2ed50d7d68dd9a0ee
Azarattum <azarattum@pm.me>
2023-04-16 12:28:58 +0700

    Implemented feed cascade trigger

--- packages/crdata/sql/cascade.sql
+++ packages/crdata/sql/cascade.sql
@@ -13,6 +13,8 @@ WHEN NOT EXISTS (
     WHERE track = OLD.track 
   UNION ALL SELECT 1 FROM playback
     WHERE track = OLD.track
+  UNION ALL SELECT 1 FROM feed 
+    WHERE track = OLD.track
   LIMIT 1
 )
 BEGIN
@@ -26,6 +28,24 @@ WHEN NOT EXISTS (
   SELECT 1 FROM library
     WHERE track = OLD.track
   UNION ALL SELECT 1 FROM playback
+    WHERE track = OLD.track
+  UNION ALL SELECT 1 FROM feed 
+    WHERE track = OLD.track
+  LIMIT 1
+)
+BEGIN
+  DELETE FROM tracks WHERE id = OLD.track;
+END;
+
+CREATE TRIGGER IF NOT EXISTS cascade_feed_tracks
+AFTER DELETE ON feed
+FOR EACH ROW
+WHEN NOT EXISTS (
+  SELECT 1 FROM library
+    WHERE track = OLD.track
+  UNION ALL SELECT 1 FROM playback
+    WHERE track = OLD.track
+  UNION ALL SELECT 1 FROM feed 
     WHERE track = OLD.track
   LIMIT 1
 )
