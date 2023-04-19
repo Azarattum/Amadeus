19591002e10b090f170c2c104675e188687311e6
Azarattum <azarattum@pm.me>
2023-04-19 20:01:34 +0700

    Reorganized UI components

--- packages/ui/component.ts
+++ packages/ui/component.ts
@@ -15,16 +15,17 @@ export { default as Virtual } from "./lib/layout/Virtual.svelte";
 export { default as Sortable } from "./lib/layout/Sortable.svelte";
 export { default as Separator } from "./lib/layout/Separator.svelte";
 
-export { default as Logo } from "./lib/primitive/Logo.svelte";
-export { default as Icon } from "./lib/primitive/Icon.svelte";
-export { default as Input } from "./lib/primitive/Input.svelte";
-export { default as Group } from "./lib/primitive/Group.svelte";
-export { default as Button } from "./lib/primitive/Button.svelte";
-export { default as Header } from "./lib/primitive/Header.svelte";
-export { default as Tooltip } from "./lib/primitive/Tooltip.svelte";
-export { default as Spinner } from "./lib/primitive/Spinner.svelte";
-export { default as Checkbox } from "./lib/primitive/Checkbox.svelte";
+export { default as Input } from "./lib/interactive/Input.svelte";
+export { default as Group } from "./lib/interactive/Group.svelte";
+export { default as Button } from "./lib/interactive/Button.svelte";
+export { default as Checkbox } from "./lib/interactive/Checkbox.svelte";
 
-export * from "./lib/elements/LightSwitch.svelte";
-export { default as Nav } from "./lib/elements/Nav.svelte";
-export { default as LightSwitch } from "./lib/elements/LightSwitch.svelte";
+export { default as Logo } from "./lib/static/Logo.svelte";
+export { default as Icon } from "./lib/static/Icon.svelte";
+export { default as Header } from "./lib/static/Header.svelte";
+export { default as Tooltip } from "./lib/static/Tooltip.svelte";
+export { default as Spinner } from "./lib/static/Spinner.svelte";
+
+export * from "./lib/composite/LightSwitch.svelte";
+export { default as Nav } from "./lib/composite/Nav.svelte";
+export { default as LightSwitch } from "./lib/composite/LightSwitch.svelte";
