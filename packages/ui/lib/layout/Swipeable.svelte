866acc25c42990cd6202be40d04ec1145ad31db2
Azarattum <azarattum@pm.me>
2023-04-22 21:07:55 +0700

    Made swipeable UI component

--- packages/ui/lib/layout/Swipeable.svelte
+++ packages/ui/lib/layout/Swipeable.svelte
@@ -1,9 +1,76 @@
 <script lang="ts">
-  //
+  import { createEventDispatcher, onMount } from "svelte";
+
+  const dispatch = createEventDispatcher<{
+    before: undefined;
+    after: undefined;
+  }>();
+
+  let before: HTMLDivElement | undefined;
+  let after: HTMLDivElement | undefined;
+  let activeBefore = false;
+  let activeAfter = false;
+  let lastDelta = 0;
+
+  function finish() {
+    if (activeBefore) dispatch("before");
+    if (activeAfter) dispatch("after");
+  }
+
+  onMount(() => {
+    const observer = new IntersectionObserver(
+      (entries) =>
+        entries.forEach((entry) => {
+          if (entry.target === before) {
+            if (activeBefore && lastDelta < 0) finish();
+            activeBefore = entry.isIntersecting;
+          } else if (entry.target === after) {
+            if (activeAfter && lastDelta > 0) finish();
+            activeAfter = entry.isIntersecting;
+          }
+        }),
+      { threshold: 0.5 }
+    );
+    if (before) observer.observe(before);
+    if (after) observer.observe(after);
+    return () => observer.disconnect();
+  });
 </script>
 
-<div>
-  <slot name="before" />
-  <slot />
-  <slot name="after" />
+<div
+  class="grid w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll overscroll-none rounded-lg transition-colors duration-300 ease-linear"
+  style="grid: auto / auto-flow 1fr 100% 1fr;"
+  class:bg-primary-600={activeBefore || activeAfter}
+  on:wheel={({ deltaX }) => (lastDelta = deltaX)}
+  on:touchend={finish}
+>
+  {#if $$slots.before}
+    <div
+      bind:this={before}
+      class="flex items-center px-4 text-content-200 transition-colors"
+      style="scroll-snap-align: none;"
+      class:text-white={activeBefore}
+    >
+      <slot name="before" />
+    </div>
+  {/if}
+  <div class="snap-center snap-always"><slot /></div>
+  {#if $$slots.after}
+    <div
+      bind:this={after}
+      class="flex items-center px-4 text-content-200 transition-colors"
+      class:text-white={activeAfter}
+    >
+      <slot name="after" />
+    </div>
+  {/if}
 </div>
+
+<style>
+  .grid::-webkit-scrollbar {
+    display: none;
+  }
+  .grid {
+    scrollbar-width: none;
+  }
+</style>
---
+++ packages/ui/stories/swipeable.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Swipeable, Stack, Icon, Card, Text } from "@amadeus-music/ui";
+</script>
+
+<Stack screen p>
+  <Swipeable>
+    <Icon name="sun" slot="before" />
+    <Card interactive sm flat>
+      <Text accent>Hello</Text>
+      <Text secondary sm>Adele</Text>
+      <div
+        slot="before"
+        class="h-[48px] w-[48px] rounded bg-gradient-to-r from-green-500 to-lime-500 shadow-lg"
+      />
+    </Card>
+    <Icon name="moon" slot="after" />
+  </Swipeable>
+</Stack>
