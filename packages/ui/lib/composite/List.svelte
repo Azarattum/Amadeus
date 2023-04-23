bf3e791673c88e72d7c9060e905f0bf54f5a14eb
Azarattum <azarattum@pm.me>
2023-04-23 10:30:34 +0700

    Fixed layout flow in list

--- packages/ui/lib/composite/List.svelte
+++ packages/ui/lib/composite/List.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { Card, Icon, Swipeable } from "../../component";
   import { createEventDispatcher } from "svelte";
-  import { scale } from "svelte/transition";
   type T = $$Generic;
 
   export let items: T[];
@@ -10,8 +9,7 @@
 
   const dispatch = createEventDispatcher<{ click: T; action: T }>();
 
-  function select(item: T) {
-    const id = key(item);
+  function select(id: ReturnType<typeof key>) {
     if (selected.has(id)) selected.delete(id);
     else selected.add(id);
     selected = selected;
@@ -19,9 +17,10 @@
 </script>
 
 {#each items as item, i (key(item))}
+  {@const id = key(item)}
   <div class="[&_hr]:!opacity-100 [&_hr]:last-of-type:!opacity-0">
     <Swipeable
-      on:after={() => select(item)}
+      on:after={() => select(id)}
       on:before={() => dispatch("action", item)}
     >
       <Card
@@ -29,28 +28,37 @@
         flat
         flow
         interactive
-        on:contextmenu={(e) => (e.preventDefault(), select(item))}
-        on:click={() =>
-          selected.size ? select(item) : dispatch("click", item)}
+        on:contextmenu={(e) => (e.preventDefault(), select(id))}
+        on:click={() => (selected.size ? select(id) : dispatch("click", item))}
       >
         <div
           class="pointer-events-none absolute inset-0 -z-50 bg-primary-200/30 opacity-0 transition-opacity"
-          class:opacity-100={selected.has(key(item))}
+          class:opacity-100={selected.has(id)}
         />
         <slot {item} index={i} />
         <slot name="before" slot="before" />
         <div
-          class="grid text-primary-600 [&>*]:col-start-1 [&>*]:row-start-1"
           slot="after"
+          class="grid text-primary-600 [&>*]:col-start-1 [&>*]:row-start-1"
         >
-          {#if selected.size}
+          <div
+            class="opacity-0 transition-opacity"
+            class:opacity-100={!selected.size}
+          >
+            <slot name="after" />
+          </div>
+          <div
+            class="opacity-0 transition-opacity"
+            class:opacity-100={selected.size}
+          >
             <Icon name="circle" />
-          {:else}
-            <div><slot name="after" /></div>
-          {/if}
-          {#if selected.has(key(item))}
-            <div class="absolute" transition:scale><Icon name="target" /></div>
-          {/if}
+          </div>
+          <div
+            class="scale-0 transition-transform"
+            class:scale-100={selected.has(id)}
+          >
+            <Icon name="target" />
+          </div>
         </div>
       </Card>
       <slot name="action" slot="before" />
