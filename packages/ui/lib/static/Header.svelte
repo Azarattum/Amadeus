9bda2c4acc305bfe46ce84e4aedfd1b5b81ec2e9
Azarattum <azarattum@pm.me>
2023-04-28 02:31:39 +0700

    Made headers use id instead of href

--- packages/ui/lib/static/Header.svelte
+++ packages/ui/lib/static/Header.svelte
@@ -2,11 +2,10 @@
   import type { Either } from "../../internal/types";
   type $$Props = Either<"sm" | "lg" | "xl"> & {
     center?: boolean;
-    href?: string;
+    id?: string;
   };
 
-  export let href = undefined as string | undefined;
-
+  export let id: string | undefined = undefined;
   export let center = false;
   export let sm = false;
   export let lg = false;
@@ -17,17 +16,17 @@
 
 <svelte:element
   this={tag}
+  {id}
   class="
   {center ? 'text-center' : 'text-left'} transition-colors
   {sm ? 'light py-2 text-xs uppercase text-content-200' : ''}
   {!sm && !lg && !xl ? 'text-lg' : ''}
   {lg ? 'text-xl' : ''}
   {xl ? 'h-11 text-2xl' : ''}
-  {href ? 'text-content-200 target:text-content [*:target_&]:text-content' : ''}
   "
 >
-  {#if href}
-    <a {href}><slot /></a>
+  {#if id}
+    <a href="#{id}"><slot /></a>
   {:else}
     <slot />
   {/if}
