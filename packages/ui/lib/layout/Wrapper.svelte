fb41664e7123577c31f2a90b1093fc3a8f1d394d
Azarattum <azarattum@pm.me>
2023-05-02 15:44:39 +0700

    Fixed document overflow issues

--- packages/ui/lib/layout/Wrapper.svelte
+++ packages/ui/lib/layout/Wrapper.svelte
@@ -17,7 +17,7 @@
 <Realm>
   <Gateway name="start" />
   <slot />
-  <div class="fixed bottom-0 z-50 flex w-full flex-col-reverse">
+  <div class="fixed bottom-0 z-50 flex w-full touch-none flex-col-reverse">
     <Gateway name="bottom" />
   </div>
   <div class="pointer-events-none fixed inset-0 contain-strict">
