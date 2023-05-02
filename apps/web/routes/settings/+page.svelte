2cf4340ab445f50f541969fbd67e08679e529cd5
Azarattum <azarattum@pm.me>
2023-05-02 15:40:48 +0700

    Made password hashing work outside secure contexts

--- apps/web/routes/settings/+page.svelte
+++ apps/web/routes/settings/+page.svelte
@@ -5,14 +5,21 @@
   let username = "";
   let password = "";
   async function login() {
-    const token = await crypto.subtle
-      .digest("SHA-1", new TextEncoder().encode(password))
-      .then((x) => Array.from(new Uint8Array(x)))
-      .then((x) => x.map((y) => y.toString(16).padStart(2, "0")).join(""));
+    const token = await hash(password);
     const protocol = globalThis.location?.protocol === "http:" ? "ws:" : "wss:";
     const url = `${protocol}//${hostname}/trpc/${username}/${token}`;
     localStorage.setItem("remote", url);
     location.reload();
+  }
+
+  function hash(text: string) {
+    if (!isSecureContext) {
+      return import("sha1-browser" as any).then((x) => x.default(text));
+    }
+    return crypto.subtle
+      .digest("SHA-1", new TextEncoder().encode(text))
+      .then((x) => Array.from(new Uint8Array(x)))
+      .then((x) => x.map((y) => y.toString(16).padStart(2, "0")).join(""));
   }
 </script>
 
