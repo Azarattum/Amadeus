84c5f3c205b7fa226f0bdb82bdc527d1c81e98c0
Azarattum <azarattum@pm.me>
2023-05-11 02:19:45 +0700

    Added expand tRPC route

---
+++ plugins/trpc/routes/expand.ts
@@ -0,0 +1,31 @@
+import {
+  Album,
+  ArtistDetails,
+  TrackDetails,
+  Unique,
+} from "@amadeus-music/protocol";
+import { info, procedure, router, expand as expandOf } from "../plugin";
+import { observable } from "@trpc/server/observable";
+import { number, object } from "superstruct";
+import { Stream, stream } from "./stream";
+
+const expandProcedure = procedure.input(
+  object({ id: number(), page: number() })
+);
+
+export const expand = router({
+  artist: expandProcedure.subscription(async ({ input: { id, page }, ctx }) => {
+    const artist = await ctx.cache().artists.get(id);
+    info(`${ctx.name} requested artist "${artist.title}".`);
+    return observable<Stream<TrackDetails, ArtistDetails>>(({ next }) => {
+      return stream(next, expandOf("artist", artist.source, page), artist);
+    });
+  }),
+  album: expandProcedure.subscription(async ({ input: { id, page }, ctx }) => {
+    const album = await ctx.cache().albums.get(id);
+    info(`${ctx.name} requested album "${album.title}".`);
+    return observable<Stream<TrackDetails, Unique<Album>>>(({ next }) => {
+      return stream(next, expandOf("album", album.source, page), album);
+    });
+  }),
+});
--- plugins/trpc/routes/index.ts
+++ plugins/trpc/routes/index.ts
@@ -1,6 +1,8 @@
 import { router } from "../plugin";
+import { expand } from "./expand";
 import { search } from "./search";
+import { next } from "./stream";
 import { sync } from "./sync";
 
-export const app = router({ sync, search });
+export const app = router({ sync, search, expand, streams: next });
 export type App = typeof app;
