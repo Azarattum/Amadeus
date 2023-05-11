0062eb98707d01c849506266b20c918212d506a2
Azarattum <azarattum@pm.me>
2023-05-11 15:02:55 +0700

    Made artist page show sources

--- apps/web/lib/ui/Artist.svelte
+++ apps/web/lib/ui/Artist.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
-  import type { ArtistCollection, TrackEntry } from "@amadeus-music/protocol";
+  import type { ArtistCollection, TrackDetails } from "@amadeus-music/protocol";
   import { Icon, Stack, Header, Text, Topbar } from "@amadeus-music/ui";
-  import { format } from "@amadeus-music/util/string";
+  import { capitalize, format } from "@amadeus-music/util/string";
   import Tracks from "$lib/ui/Tracks.svelte";
   import Avatar from "./Avatar.svelte";
   import { search } from "$lib/data";
   import { match } from "$lib/util";
 
-  export let selected = new Set<TrackEntry>();
-  export let info: ArtistCollection | undefined = undefined;
+  export let selected = new Set<TrackDetails>();
+  export let info: Partial<ArtistCollection> | undefined = undefined;
+  export let tracks = info?.tracks;
 
-  $: tracks = info?.tracks.filter(match($search));
+  $: filtered = tracks?.filter(match($search));
+  $: sources = [
+    ...new Set<string>(
+      JSON.parse(info?.source || "[]")
+        .map((x: string) => x.split("/")[0])
+        .filter((x: string) => x)
+    ),
+  ].join(", ");
 </script>
 
 <Topbar title={info?.title || ""}>
@@ -19,21 +27,25 @@
     <Stack gap>
       <Header loading={!info}>{info?.title}</Header>
       <Stack x gap="lg">
-        <Text secondary loading={!info}>
-          <Icon name="note" sm />
-          {info?.count}
-        </Text>
-        <Text secondary loading={!info}>
-          <Icon name="clock" sm />
-          {format(info?.length || 0)}
-        </Text>
+        {#if !info || (info.count != null && info.length != null)}
+          <Text secondary loading={!info}>
+            <Icon name="note" sm />
+            {info?.count}
+          </Text>
+          <Text secondary loading={!info}>
+            <Icon name="clock" sm />
+            {format(info?.length || 0)}
+          </Text>
+        {:else}
+          <Text secondary>{capitalize(sources)}</Text>
+        {/if}
       </Stack>
     </Stack>
   </Stack>
 </Topbar>
 <!-- /// TODO: albums -->
 
-<Tracks fixed {tracks} on:edit on:click on:action bind:selected>
+<Tracks fixed tracks={filtered} on:edit on:click on:action on:end bind:selected>
   <slot name="action" slot="action" />
   <slot />
 </Tracks>
