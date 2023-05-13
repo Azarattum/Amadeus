7608d61bc6c602db21d5d3b7ab9b2e8d81ba7b79
Azarattum <azarattum@pm.me>
2023-05-14 00:49:34 +0700

    Refactored some logic in yandex plugin

--- plugins/yandex/index.ts
+++ plugins/yandex/index.ts
@@ -20,7 +20,7 @@ import {
   lyrics,
   match,
 } from "./types";
-import { format } from "@amadeus-music/protocol";
+import { format, map } from "@amadeus-music/core";
 import { auth, header, transform } from "./meta";
 import { createHash } from "node:crypto";
 
@@ -28,7 +28,7 @@ init(function* ({ yandex: { token } }) {
   if (!token) throw "Plugin disabled! No token found!";
   this.connect.baseURL = "wss://uniproxy.alice.yandex.net/uni.ws";
   this.fetch.baseURL = "https://api.music.yandex.net/";
-  this.fetch.params = { age: "0" };
+  this.fetch.params = { page: "0" };
   this.fetch.headers = {
     "User-Agent": "Yandex-Music-API",
     Authorization: `OAuth ${token}`,
@@ -39,17 +39,17 @@ search(function* (type, query, page) {
   /// Properly support other `type`s!
   if (type !== "track") return;
 
-  for (let i = 0; ; ) {
+  for (let i = 0; ; i++) {
     const params = {
       type,
       text: query,
       nococrrect: "false",
-      page: (i++).toString(),
-      "page-size": page.toString(),
+      page: i,
+      "page-size": page,
     };
 
     const { result } = yield* fetch("search", { params }).as(results);
-    if (!result.tracks?.results.length) break;
+    if (!result.tracks || result.tracks.results.length < page) break;
     yield* result.tracks.results.map(toTrack);
   }
 });
@@ -72,42 +72,30 @@ expand(function* (type, source, page) {
     const { result } = yield* fetch(`albums/${id}/with-tracks`).as(volumes);
     yield* result.volumes.flat().map(toTrack);
   } else if (type === "artist") {
-    for (let i = 0; ; ) {
+    for (let i = 0; ; i++) {
       const url = `artists/${id}/tracks`;
-      const params = {
-        page: (i++).toString(),
-        "page-size": page.toString(),
-      };
+      const params = { page: i++, "page-size": page };
       const { result } = yield* fetch(url, { params }).as(tracks);
-      if (!result.tracks?.length) break;
+      if (!result.tracks || result.tracks.length < page) break;
       yield* result.tracks.map(toTrack);
     }
   }
 });
 
 relate(function* (type, to, _) {
-  let id = to.source.match(/yandex\/([0-9]+)/)?.[1];
-  if (type === "track") {
-    if (!id) {
-      const params = { text: format(to), type, "page-size": "1" };
-      const { result } = yield* fetch("search", { params }).as(results);
-      id = result.tracks?.results[0]?.id.toString();
-      if (!id) return;
-    }
-    const { result } = yield* fetch(`tracks/${id}/similar`).as(similar);
-    yield* result.similarTracks.map(toTrack);
-  }
   /// Properly support other `type`s!
+  if (type !== "track") return;
+  const id = yield* identify(to, type);
+  if (!id) return;
+
+  const { result } = yield* fetch(`tracks/${id}/similar`).as(similar);
+  yield* result.similarTracks.map(toTrack);
 });
 
 transcribe(function* (track) {
-  let id = track.source.match(/yandex\/([0-9]+)/)?.[1];
-  if (!id) {
-    const params = { text: format(track), type: "track", "page-size": "1" };
-    const { result } = yield* fetch("search", { params }).as(results);
-    id = result.tracks?.results[0]?.id.toString();
-    if (!id) return;
-  }
+  const id = yield* identify(track, "track");
+  if (!id) return;
+
   const { result } = yield* fetch(`tracks/${id}/supplement`).as(lyrics);
   if (result.lyrics) yield result.lyrics.fullLyrics;
 });
@@ -124,3 +112,17 @@ recognize(function* (stream) {
   if ("data" in directive.payload) yield toTrack(directive.payload.data.match);
   yield* connection.close();
 });
+
+function* identify(to: { source: string }, type: "track" | "artist" | "album") {
+  const regex = /yandex\/([0-9]+)/;
+  return (
+    to.source.match(regex)?.[1] ||
+    (yield* map(
+      search.where("yandex")(type as any, format(to), 1),
+      function* (x) {
+        if (x.progress >= 1) x.close();
+        return x.items[0]?.source.match(regex)?.[1];
+      }
+    )).find((x) => x)
+  );
+}
