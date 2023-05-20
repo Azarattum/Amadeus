a1fa7ac0f3fe299ac1d9684d533a386ced5dded0
Azarattum <azarattum@pm.me>
2023-05-21 00:14:02 +0700

    Adapted card component to the new protocol

--- apps/web/lib/ui/Card.svelte
+++ apps/web/lib/ui/Card.svelte
@@ -1,50 +1,42 @@
 <script lang="ts">
+  import type { Album, Artist, Playlist } from "@amadeus-music/protocol";
   import { Card, Header, Stack, Text, Icon } from "@amadeus-music/ui";
   import { format } from "@amadeus-music/util/string";
   import Avatar from "./Avatar.svelte";
 
-  type Info = {
-    tracks?: { album: { art?: string } }[];
-    artists?: { title: string }[];
-    album?: { art: string };
-    length?: number;
-    count?: number;
-    title: string;
-    art?: string;
-  };
-  export let playlist: Info | undefined | true = undefined;
-  export let artist: Info | undefined | true = undefined;
-  export let album: Info | undefined | true = undefined;
+  export let playlist: Playlist | undefined | true = undefined;
+  export let artist: Artist | undefined | true = undefined;
+  export let album: Album | undefined | true = undefined;
 
   export let href: string | undefined = undefined;
 
   const ok = (x: any): x is object => typeof x === "object";
-  $: item = (playlist || artist || album) as Info | true;
+  $: item = playlist || artist || album;
   $: loading = item === true;
 
   $: title = ok(item) ? item.title : "Loading...";
-  $: length = ok(item) ? item.length || undefined : 0;
-  $: count = ok(item) ? item.count || undefined : 0;
-  $: art = ok(item)
-    ? item.art
-      ? item
-      : Array.isArray(item.tracks)
-      ? item.tracks
-      : item
-    : undefined;
+  $: art = ok(item) ? item : undefined;
 </script>
 
 <Card interactive={!!href && !loading} {href}>
   <Header center {loading}>{title}</Header>
-  {#if album && ok(item) && item.artists}
+  {#if album && ok(item) && "artists" in item}
     <Text secondary {loading}>
       {item.artists.map((x) => x.title).join(", ")}
     </Text>
-  {:else if count != null && length != null}
+  {:else if ok(item) && item.collection}
     <Stack justify x gap="lg">
-      <Text secondary {loading}><Icon name="note" sm /> {count}</Text>
-      <Text secondary {loading}><Icon name="clock" sm /> {format(length)}</Text>
+      <Text secondary {loading}>
+        <Icon name="note" sm />
+        {item.collection.size}
+      </Text>
+      <Text secondary {loading}>
+        <Icon name="clock" sm />
+        {format(item.collection.duration)}
+      </Text>
     </Stack>
+  {:else if !artist}
+    <Text secondary {loading}>Loading...</Text>
   {/if}
   <Avatar round={!!artist} of={art} slot="after" />
 </Card>
