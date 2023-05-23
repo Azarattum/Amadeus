cdbeda75aef4636fca8bee02db438e0ee3d967cb
Azarattum <azarattum@pm.me>
2023-05-23 13:08:50 +0700

    Added abort support for ffmpeg

--- apps/core/data/ffmpeg.ts
+++ apps/core/data/ffmpeg.ts
@@ -1,6 +1,7 @@
 import type { Meta } from "@amadeus-music/protocol";
 import { err } from "../status/log";
 import ffmpeg from "fluent-ffmpeg";
+import { context } from "libfun";
 
 function reencode(
   url: string,
@@ -8,7 +9,7 @@ function reencode(
   format: "opus" | "ogg" | "caf" = "opus"
 ) {
   const artists = metadata.artists?.map((x) => x.title).join(", ");
-  let stream = ffmpeg(url).on("error", err.bind({ group: "ffmpeg" }));
+  let stream = ffmpeg(url).on("error", error);
   if (metadata.album?.title) {
     stream = stream.outputOptions("-metadata", `Album=${metadata.album.title}`);
   }
@@ -18,16 +19,29 @@ function reencode(
   if (artists?.length) {
     stream = stream.outputOptions("-metadata", `Artist=${artists}`);
   }
+
+  context?.signal?.addEventListener("abort", () => stream.kill("SIGINT"), {
+    once: true,
+  });
   return stream.audioCodec("libopus").toFormat(format).pipe();
 }
 
 function resize(url: string, size: number) {
-  return ffmpeg(url)
-    .on("error", err.bind({ group: "ffmpeg" }))
+  const stream = ffmpeg(url)
+    .on("error", error)
     .addOptions("-vf", "crop=ih:ih")
     .size(`${size}x${size}`)
-    .toFormat("mjpeg")
-    .pipe();
+    .toFormat("mjpeg");
+
+  context?.signal?.addEventListener("abort", () => stream.kill("SIGINT"), {
+    once: true,
+  });
+  return stream.pipe();
+}
+
+function error(reason: unknown) {
+  if (String(reason).includes("received signal 2")) return;
+  err.bind({ group: "ffmpeg" })(reason);
 }
 
 export { reencode, resize };
