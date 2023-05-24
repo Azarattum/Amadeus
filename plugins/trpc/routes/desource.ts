531dc34ac9e132a3bce346b7cefddc535d1f7620
Azarattum <azarattum@pm.me>
2023-05-25 01:53:06 +0700

    Added transcribe and desource log messages

--- plugins/trpc/routes/desource.ts
+++ plugins/trpc/routes/desource.ts
@@ -1,7 +1,8 @@
-import { procedure, desource as desourceOf } from "../plugin";
-import { track } from "@amadeus-music/protocol";
+import { procedure, desource as desourceOf, info } from "../plugin";
+import { format, track } from "@amadeus-music/protocol";
 import { first } from "@amadeus-music/core";
 
-export const desource = procedure.input(track).query(async ({ input }) => {
+export const desource = procedure.input(track).query(async ({ input, ctx }) => {
+  info(`${ctx.name} requested source of "${format(input)}".`);
   return await first(desourceOf(input));
 });
--- plugins/trpc/routes/transcribe.ts
+++ plugins/trpc/routes/transcribe.ts
@@ -1,7 +1,10 @@
-import { procedure, transcribe as transcribeOf } from "../plugin";
-import { track } from "@amadeus-music/protocol";
+import { info, procedure, transcribe as transcribeOf } from "../plugin";
+import { format, track } from "@amadeus-music/protocol";
 import { first } from "@amadeus-music/core";
 
-export const transcribe = procedure.input(track).query(async ({ input }) => {
-  return await first(transcribeOf(input));
-});
+export const transcribe = procedure
+  .input(track)
+  .query(async ({ input, ctx }) => {
+    info(`${ctx.name} requested lyrics for "${format(input)}".`);
+    return await first(transcribeOf(input));
+  });
