2d8c1b21164bd894341c95e2f7bbb431159fa16a
Azarattum <azarattum@pm.me>
2023-05-24 23:52:52 +0700

    Fixed tailwind nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,10 +3,8 @@ const resolve = (path) => require("path").resolve(__dirname, path);
 module.exports = {
   plugins: {
     "postcss-import": {},
-    "postcss-nested": {},
-    tailwindcss: {
-      config: resolve("tailwind.config.cjs"),
-    },
+    "tailwindcss/nesting": {},
+    tailwindcss: { config: resolve("tailwind.config.cjs") },
     autoprefixer: {},
     cssnano: {},
   },
