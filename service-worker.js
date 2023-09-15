ef8c517b40bf742b1836c5ae6e1c52a827540193
Azarattum <Azarattum@users.noreply.github.com>
2023-09-15 06:32:47 +0000

    deploy: bbe7ae535e920a081eee17dc1a5e4247dd97bc6f

--- service-worker.js
+++ service-worker.js
@@ -1,72 +1,72 @@
-const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), u = [
-  e + "/_app/immutable/entry/start.ce9b72fa.js",
-  e + "/_app/immutable/assets/crsqlite.0980e00c.wasm",
-  e + "/_app/immutable/entry/app.5ec30f37.js",
-  e + "/_app/immutable/nodes/0.849e1f54.js",
-  e + "/_app/immutable/nodes/1.e45340d3.js",
-  e + "/_app/immutable/nodes/10.7b124ae7.js",
-  e + "/_app/immutable/nodes/11.0fd7a5a3.js",
-  e + "/_app/immutable/nodes/2.497e5e11.js",
-  e + "/_app/immutable/nodes/3.d29e1c1f.js",
-  e + "/_app/immutable/nodes/4.9280f1f8.js",
-  e + "/_app/immutable/nodes/5.d4f5388d.js",
-  e + "/_app/immutable/nodes/6.912fe3a6.js",
-  e + "/_app/immutable/nodes/7.b6fe6fc8.js",
-  e + "/_app/immutable/nodes/8.c21a8a66.js",
-  e + "/_app/immutable/nodes/9.6820c7d1.js",
-  e + "/_app/immutable/assets/Player.c0ee8b34.css",
-  e + "/_app/immutable/assets/Spinner.e3cd6353.css",
-  e + "/_app/immutable/chunks/Collection.0764a713.js",
-  e + "/_app/immutable/chunks/Input.66097ce7.js",
-  e + "/_app/immutable/chunks/Overview.0cec3efa.js",
-  e + "/_app/immutable/chunks/Player.f344efcc.js",
-  e + "/_app/immutable/chunks/Spacer.98ca708d.js",
-  e + "/_app/immutable/chunks/Spinner.svelte_svelte_type_style_lang.1007239d.js",
-  e + "/_app/immutable/chunks/Track.f4dc3669.js",
-  e + "/_app/immutable/chunks/autoscroll.33fd421b.js",
-  e + "/_app/immutable/chunks/control.f5b05b5f.js",
-  e + "/_app/immutable/chunks/index.84a1bd3e.js",
-  e + "/_app/immutable/chunks/paths.9124caa8.js",
-  e + "/_app/immutable/chunks/singletons.0780fce8.js",
-  e + "/_app/immutable/chunks/stores.1d34bc11.js",
-  e + "/_app/immutable/chunks/stream.7b84773f.js",
-  e + "/_app/immutable/chunks/util.245348a2.js"
-], o = [
-  e + "/CNAME",
-  e + "/images/favicon.png",
-  e + "/images/logo-180.webp",
-  e + "/images/logo-512.webp",
-  e + "/images/logo.svg",
-  e + "/images/splash-5.webp",
-  e + "/images/splash-8.webp",
-  e + "/images/splash-plus.webp",
-  e + "/images/splash-x.webp",
-  e + "/manifest.json"
-], b = "1687077198082", c = `cache-${b}`, m = [...u, ...o], p = self;
-p.addEventListener("fetch", (a) => {
-  if (a.request.method !== "GET")
+const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), u = [
+  a + "/_app/immutable/entry/start.403048da.js",
+  a + "/_app/immutable/assets/crsqlite.0980e00c.wasm",
+  a + "/_app/immutable/entry/app.e9f02e15.js",
+  a + "/_app/immutable/nodes/0.35829ef9.js",
+  a + "/_app/immutable/nodes/1.cb3ec366.js",
+  a + "/_app/immutable/nodes/10.5e276b8b.js",
+  a + "/_app/immutable/nodes/11.0e47e2ee.js",
+  a + "/_app/immutable/nodes/2.497e5e11.js",
+  a + "/_app/immutable/nodes/3.2aa08aaa.js",
+  a + "/_app/immutable/nodes/4.e9b3aba6.js",
+  a + "/_app/immutable/nodes/5.ce2fe09a.js",
+  a + "/_app/immutable/nodes/6.1ce83989.js",
+  a + "/_app/immutable/nodes/7.da826cca.js",
+  a + "/_app/immutable/nodes/8.815791a1.js",
+  a + "/_app/immutable/nodes/9.9c4a84ea.js",
+  a + "/_app/immutable/assets/Player.c0ee8b34.css",
+  a + "/_app/immutable/assets/Spinner.e3cd6353.css",
+  a + "/_app/immutable/chunks/Collection.80a6433f.js",
+  a + "/_app/immutable/chunks/Input.9f31e30a.js",
+  a + "/_app/immutable/chunks/Overview.1ded8ffd.js",
+  a + "/_app/immutable/chunks/Player.9458476b.js",
+  a + "/_app/immutable/chunks/Spacer.98ca708d.js",
+  a + "/_app/immutable/chunks/Spinner.svelte_svelte_type_style_lang.c059ec09.js",
+  a + "/_app/immutable/chunks/Track.23aa9abb.js",
+  a + "/_app/immutable/chunks/autoscroll.f36ad9e7.js",
+  a + "/_app/immutable/chunks/control.f5b05b5f.js",
+  a + "/_app/immutable/chunks/index.84a1bd3e.js",
+  a + "/_app/immutable/chunks/paths.8460f9e3.js",
+  a + "/_app/immutable/chunks/singletons.f64675c0.js",
+  a + "/_app/immutable/chunks/stores.dd7b98e4.js",
+  a + "/_app/immutable/chunks/stream.0890742a.js",
+  a + "/_app/immutable/chunks/util.f1fbddbb.js"
+], b = [
+  a + "/CNAME",
+  a + "/images/favicon.png",
+  a + "/images/logo-180.webp",
+  a + "/images/logo-512.webp",
+  a + "/images/logo.svg",
+  a + "/images/splash-5.webp",
+  a + "/images/splash-8.webp",
+  a + "/images/splash-plus.webp",
+  a + "/images/splash-x.webp",
+  a + "/manifest.json"
+], o = "1694759544521", c = `cache-${o}`, m = [...u, ...b], p = self;
+p.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET")
     return;
   async function t() {
-    const s = await caches.open(c), { hash: l, pathname: n } = new URL(a.request.url);
+    const s = await caches.open(c), { hash: l, pathname: n } = new URL(e.request.url);
     if (new URLSearchParams(l.slice(1)).get("cache"), m.includes(n)) {
       const i = await s.match(n);
       if (i)
         return i;
     }
-    return await fetch(a.request);
+    return await fetch(e.request);
   }
-  a.respondWith(t());
+  e.respondWith(t());
 });
-p.addEventListener("install", (a) => {
+p.addEventListener("install", (e) => {
   async function t() {
     await (await caches.open(c)).addAll(m);
   }
-  a.waitUntil(t());
+  e.waitUntil(t());
 });
-p.addEventListener("activate", (a) => {
+p.addEventListener("activate", (e) => {
   async function t() {
     for (const s of await caches.keys())
       s.startsWith("cache-") && s !== c && await caches.delete(s);
   }
-  a.waitUntil(t());
+  e.waitUntil(t());
 });
