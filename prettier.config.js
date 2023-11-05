42f4a83dcc7dda6f3153ac2cd2aa451345e97c24
Azarattum <azarattum@pm.me>
2023-11-05 13:53:04 +0700

    Upgraded dependencies

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: [
+    "prettier-plugin-svelte",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-sort-imports",
+  ],
+  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+};
