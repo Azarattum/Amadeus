df415a89a739815a5212cab25d04539dad91aec3
Azarattum <azarattum@pm.me>
2023-12-01 21:30:21 +0700

    Reimplemented morph as a component

---
+++ packages/ui/lib/layout/Words.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let from: string | undefined;
+</script>
+
+{#if from}
+  {#each from.split(" ").filter((x) => x) as word}
+    <span style="line-height: 100%;" class="inline-block">{word}</span>{" "}
+  {/each}
+{/if}
