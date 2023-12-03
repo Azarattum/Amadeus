5128026ef413dfa2ac58416a8259db5854970dea
Azarattum <azarattum@pm.me>
2023-12-03 22:14:55 +0700

    Added classes passthrough to text

--- packages/ui/lib/static/Text.svelte
+++ packages/ui/lib/static/Text.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import { type Classes, tw } from "../../internal/tailwind";
+
+  let classes: Classes = "";
+  export { classes as class };
   export let secondary = false;
   export let loading = false;
   export let accent = false;
@@ -7,9 +11,10 @@
 </script>
 
 <span
-  class="w-max max-w-full truncate rounded-md text-left [&>*]:align-bottom
-  {loading ? 'animate-pulse bg-highlight text-transparent' : ''}
-  {accent ? 'font-medium' : 'font-normal'}"
+  class={tw`w-max max-w-full truncate rounded-md text-left [&>*]:align-bottom
+  ${loading && "animate-pulse bg-highlight text-transparent"}
+  ${accent ? "font-medium" : "font-normal"}
+  ${classes}`}
   class:text-content-200={secondary}
   class:ml-4={indent}
   class:text-sm={sm}
