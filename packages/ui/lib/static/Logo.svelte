c8dfd5f7be018920f6c53b23db2317a403d09cf3
Azarattum <azarattum@pm.me>
2024-02-23 13:33:52 +0700

    Moved icons folder

--- packages/ui/lib/static/Logo.svelte
+++ packages/ui/lib/static/Logo.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { Icon } from "../../component";
+  import Logo from "../../icons/amadeus.svg?raw";
   export let sm: boolean | "auto" = false;
 </script>
 
 <div class="flex items-center py-8 text-content-200">
-  <Icon of="amadeus" xl />
+  <div class="size-11">{@html Logo}</div>
   <span
     class="h-11 select-none text-2xl font-light"
     class:xl:block={sm === "auto"}
